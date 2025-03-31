10776a6b67f293eee26d725a5e268443439967c6
o.urregov <o.urregov@uniandes.edu.co>
2025-03-31 23:35:43 +0200

    First try of importing font

--- src/main/java/frontend/SPCApp.java
+++ src/main/java/frontend/SPCApp.java
@@ -1,5 +1,8 @@
 package frontend;
 
+import javafx.scene.text.Font;
+import org.kordamp.ikonli.javafx.FontIcon;
+import org.kordamp.ikonli.fontawesome5.FontAwesomeSolid;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
@@ -11,11 +14,13 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.FileNotFoundException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 
 public class SPCApp extends Application {
 
 
-    Button dashboardButton = new Button("Dashboard");
+    Button dashboardButton = new Button();
     Button advancedButton = new Button("Advanced");
     Button feedbackButton = new Button("Feedback");
 
@@ -24,6 +29,22 @@ public class SPCApp extends Application {
 
     @Override
     public void start(Stage stage) {
+
+        String fontPath = getClass().getResource("/fonts/Poppins-ThinItalic.ttf").toExternalForm();
+        fontPath = URLDecoder.decode(fontPath, StandardCharsets.UTF_8);
+
+        System.out.println("Font path: " + fontPath); // Debugging
+
+        //Font.loadFont(fontPath, 10);
+
+        //Font.getFamilies().forEach(System.out::println);
+        //We set the CSS classes to the components:
+
+
+
+        dashboardButton = new Button("Dashboard", new FontIcon(FontAwesomeSolid.CHART_BAR));
+        dashboardButton.setGraphicTextGap(20);
+        dashboardButton.getStyleClass().add("poppins-italic-text");
 
         Image logo = new Image(getClass().getResource("/images/logo.png").toExternalForm());
         ImageView logoImageView = new ImageView(logo);
@@ -51,6 +72,7 @@ public class SPCApp extends Application {
 
 
         Scene scene = new Scene(sideBar, 250, 700);
+        scene.getStylesheets().add(getClass().getResource("/styles/styles.css").toExternalForm());
         stage.setTitle("Smart Plant Care");
         stage.setScene(scene);
         stage.show();
