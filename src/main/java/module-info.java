0d69c99c09d1919188e6fbc54992cd24367b99ae
o.urregov <o.urregov@uniandes.edu.co>
2025-04-07 19:15:08 +0200

    Chart panel

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,11 @@
 module org.example.smartplantcare {
-    requires javafx.controls;
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
     requires org.kordamp.ikonli.fontawesome5;
+    requires eu.hansolo.tilesfx;
 
     opens org.example.smartplantcare to javafx.fxml;
     exports org.example.smartplantcare;
