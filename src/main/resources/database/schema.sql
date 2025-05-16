2a0a6e849e6c3616583bb979d0b42d678464fff3
Eren Gokirmak <sukruerengokirmak@gmail.com>
2025-05-16 11:54:58 +0200

    Change database schema to allow devices and profiles

--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -7,22 +7,34 @@ create table measurement
     id INTEGER PRIMARY KEY,
     device_id REFERENCES device(id),
     timestamp TIMESTAMP,
-    light    FLOAT,
+    light    INTEGER,
     temp     FLOAT,
-    water    FLOAT,
+    water    INTEGER,
     humidity FLOAT
 );
 
 create table device
 (
-    id TEXT PRIMARY KEY ,
-    name TEXT
+    id TEXT PRIMARY KEY,
+    name TEXT,
+    active_profile REFERENCES profile(id)
 );
 
-/* THIS IS INCOMPLETE */
 create table profile
 (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    ideal_temp FLOAT
-)
+    ideal_temp FLOAT,
+    max_temp FLOAT,
+    min_temp FLOAT,
+    ideal_light INTEGER,
+    max_light INTEGER,
+    min_light INTEGER,
+    ideal_water INTEGER,
+    max_water INTEGER,
+    min_water INTEGER,
+    ideal_humidity FLOAT,
+    max_humidity FLOAT,
+    min_humidity FLOAT,
+    expiry_date TIMESTAMP
+);
